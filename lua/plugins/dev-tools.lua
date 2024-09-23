de847fb2c20b078cd8b1e4e99537a73c42ebae4e
Adel Sehic <adelsehic95@gmail.com>
2024-09-23 23:13:38 +0200

    - The name, is the harpoonagen

--- lua/plugins/dev-tools.lua
+++ lua/plugins/dev-tools.lua
@@ -235,5 +235,21 @@ return {
       live_rename.setup()
       vim.keymap.set("n", "<leader>R", live_rename.map({ insert = true }), { desc = "LSP rename" })
     end
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    init = function ()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+      vim.keymap.set("n", "<leader>ha", function () harpoon:list():add() end, { desc = "[H]arpoon [A]dd to list"})
+      vim.keymap.set("n", "<leader>hh", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "[HH]arpoon show"})
+
+      vim.keymap.set("n", "<C-1>", function () harpoon:list():select(1) end)
+      vim.keymap.set("n", "<C-2>", function () harpoon:list():select(2) end)
+      vim.keymap.set("n", "<C-3>", function () harpoon:list():select(3) end)
+      vim.keymap.set("n", "<C-4>", function () harpoon:list():select(4) end)
+    end
   }
 }
