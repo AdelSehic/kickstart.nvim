ce1747e00d5dfb7fb912fd37befb4aa0f3d52acd
Adel Sehic <adelsehic95@gmail.com>
2024-09-26 21:37:10 +0200

    - Removed noice nvim, added lualine

--- lua/plugins/ui-plugins.lua
+++ lua/plugins/ui-plugins.lua
@@ -1,31 +1,3 @@
-local colors = {
-  blue = '#80a0ff',
-  cyan = '#79dac8',
-  black = '#080808',
-  white = '#c6c6c6',
-  red = '#ff5189',
-  violet = '#d183e8',
-  grey = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.white },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.white },
-  },
-}
-
 return {
   -- :Telescope colorscheme`.
   {
@@ -73,41 +45,6 @@ return {
       },
     },
   },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    init = function()
-      require('lualine').setup {
-        options = {
-          theme = bubbles_theme,
-          component_separators = '',
-          section_separators = { left = '', right = '' },
-        },
-        sections = {
-          lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-          lualine_b = { 'filename', 'branch' },
-          lualine_c = {
-            '%=', --[[ add your center compoentnts here in place of this comment ]]
-          },
-          lualine_x = {},
-          lualine_y = { 'filetype', 'progress' },
-          lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 2 },
-          },
-        },
-        inactive_sections = {
-          lualine_a = { 'filename' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'location' },
-        },
-        tabline = {},
-        extensions = {},
-      }
-    end,
-  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   {
@@ -129,30 +66,72 @@ return {
     },
   },
   -- lazy.nvim
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = true, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-    },
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'rcarriga/nvim-notify',
-    },
-  },
+  -- {
+  --   'folke/noice.nvim',
+  --   -- event = 'VeryLazy',
+  --   opts = {
+  --     lsp = {
+  --       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --       override = {
+  --         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+  --         ['vim.lsp.util.stylize_markdown'] = true,
+  --         ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+  --       },
+  --     },
+  --     -- you can enable a preset for easier configuration
+  --     presets = {
+  --       bottom_search = true, -- use a classic bottom cmdline for search
+  --       command_palette = true, -- position the cmdline and popupmenu together
+  --       long_message_to_split = true, -- long messages will be sent to a split
+  --       inc_rename = true, -- enables an input dialog for inc-rename.nvim
+  --       lsp_doc_border = false, -- add a border to hover docs and signature help
+  --     },
+  --   },
+  --   dependencies = {
+  --     'MunifTanjim/nui.nvim',
+  --     'rcarriga/nvim-notify',
+  --   },
+  --   init = function()
+  --     require('noice').setup {
+  --       views = {
+  --         cmdline_popup = {
+  --           position = {
+  --             row = 5,
+  --             col = '50%',
+  --           },
+  --           size = {
+  --             width = 60,
+  --             height = 'auto',
+  --           },
+  --         },
+  --         popupmenu = {
+  --           relative = 'editor',
+  --           position = {
+  --             row = 8,
+  --             col = '50%',
+  --           },
+  --           size = {
+  --             width = 60,
+  --             height = 10,
+  --           },
+  --           border = {
+  --             style = 'rounded',
+  --             padding = { 0, 1 },
+  --           },
+  --           win_options = {
+  --             winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+  --           },
+  --         },
+  --       },
+  --
+  --       -- @recording in notification box
+  --       routes = {
+  --         {
+  --           view = 'notify',
+  --           filter = { event = 'msg_showmode' },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
